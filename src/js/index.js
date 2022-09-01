bf2711e9bd0c78f6ab8bcfb781a1aa89f2eee7f5
Olajide Ayeola <olajideayeola@gmail.com>
2022-09-01 14:59:14 +0100

    After addiiton of responsive about page

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+var explore = document.getElementById("exploremenu")
+
+function showExploreMenu(){
+    explore.style.display = "flex"
+    
+
+}
+
+var viewhome = document.getElementById("navLinks")
+
+function showMenu(){
+                    
+    viewhome.style.right ="0"
+
+}
+function hideMenu(){
+    viewhome.style.right ="-200px"
+}
