53ae3b0065f701a5b3177cc2342558a5533672d2
Olajide Ayeola <olajideayeola@gmail.com>
2022-09-12 12:58:37 +0100

    After addition of responsive sidebar

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,39 @@
-var explore = document.getElementById("exploremenu")
+// var explore = document.getElementById("exploremenu")
 
-function showExploreMenu(){
-    explore.style.display = "flex"
+// function showExploreMenu(){
+//     explore.style.display = "flex"
     
 
-}
+// }
 
-var viewhome = document.getElementById("navLinks")
+// var viewhome = document.getElementById("navLinks")
+
+// function showMenu(){
+                    
+//     viewhome.style.right ="0"
+
+// }
+// function hideMenu(){
+//     viewhome.style.right ="-200px"
+// }
+
+
+var menubutton = document.getElementById("menu")
+var cancelbutton = document.getElementById("cancel")
+var verticalnav = document.getElementById("actualnav")
 
 function showMenu(){
-                    
-    viewhome.style.right ="0"
+    cancelbutton.style.display = "block"
+    menubutton.style.display = "none"
+    verticalnav.style.transform = "translateX(200px)"
+    verticalnav.style.transition = "1s"
 
 }
+
 function hideMenu(){
-    viewhome.style.right ="-200px"
+    menubutton.style.display = "block"
+    cancelbutton.style.display = "none"
+    verticalnav.style.transform = "translateX(-200px)"
+    verticalnav.style.transition = "1.5s"
+    // verticalnav.style.transition = "2s"
 }
